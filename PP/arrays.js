063d114d6f7907a52a9b94d0403fe3a41b100462
Adam Zdravkovic <adambusiness24@gmail.com>
2024-12-18 20:23:43 -0800

    Branch Arrays completed

---
+++ PP/arrays.js
@@ -0,0 +1,44 @@
+// Task 1
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+console.log(months[5], months[9], months[0]);
+
+
+// Task 2
+
+var days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+
+console.log(days[6]);
+
+
+// Task 3
+
+var numbers = [2,-4,5,-2,-11];
+
+console.log(numbers[1], numbers[3], numbers[4]);
+
+// Task 4
+
+var numbers1 = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+
+console.log(numbers1[0],numbers1[3],numbers1[6]);
+
+
+// Task 5
+
+var example = [
+    [1, 2, 1, 24],
+    [8, 11, 9, 4],
+    [7, 0, 7, 27],
+    [7, 4, 28, 14],
+    [3, 10, 26, 7]
+];
+
+console.log(example[0][3]);
+
+
+// Task 6
+
+console.log(example[2]);
+console.log(example[2][1]);
