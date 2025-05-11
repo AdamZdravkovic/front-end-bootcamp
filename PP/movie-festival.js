fabc057d19e28727efe7e800b855d363ad94f0b5
Adam Zdravkovic <adambusiness24@gmail.com>
2025-05-11 12:37:57 -0700

    Movie Festival Updated

--- PP/movie-festival.js
+++ PP/movie-festival.js
@@ -51,34 +51,53 @@ function Festival(name, amountTotal) {
     this.listTotal = [];
     this.amountTotal = amountTotal;
     this.addProgram = function(program) {
+        let genreCount = 0;
+
         return this.listTotal.push(program);
     };
     this.getData = function() {
         let totalMovies = 0;
+        let programDataList = [];
 
         for (let i = 0; i < this.listTotal.length; i++) {
+            const program = this.listTotal[i];
             totalMovies += this.listTotal[i].list.length;
-        };
-
-        const programDate = this.listTotal.date;
-        let programDuration = 0;
-
-        for (let i = 0; this.listTotal.length; i++) {
-            programDuration += this.listTotal.list.length;
+            programDataList.push(this.listTotal[i].getData());
 
         };
+
+        return `${this.name} festival has ${totalMovies} movie titles\n` + programDataList.join('\n');
     };
 };
 
+function createMovie(title, length, genreName) {
+    const genre = new Genre(genreName);
+    const movie = new Movie(title, genre, length);
+    return movie;
+};
 
+function createProgram(date) {
+    const program = new Program(date);
+    return program;
+};
 
+const festival = new Festival("Weekend", 3);
 
+const program1 = createProgram("2017-10-28");
+const program2 = createProgram("2017-10-29");
 
+const movie1 = createMovie("Spider-Man: Homecoming", 133, "action");
+const movie2 = createMovie("War for the Planet of the Apes", 140, "drama");
+const movie3 = createMovie("The Dark Tower", 95, "western");
+const movie4 = createMovie("Deadpool", 108, "comedy");
 
+program1.addMovie(movie1);
+program1.addMovie(movie2);
+program1.addMovie(movie3);
+program2.addMovie(movie4);
 
+festival.addProgram(program1);
+festival.addProgram(program2);
 
-
-
-
-
+console.log(festival.getData());
 })();
