dbfe0dbd30cc85c3b82685ef75bb606e8f2882f9
Adam Zdravkovic <adambusiness24@gmail.com>
2025-05-19 11:43:13 -0700

    OOP Exercises Completed

--- PP/Completed Exercises/OOP Exercises/bookmakers.js
+++ PP/Completed Exercises/OOP Exercises/bookmakers.js
@@ -1,73 +1,100 @@
 (function () {
     "use strict";
 
+    // Constructor for Country with name, odds, and continent properties
     function Country(name, odds, continent) {
         this.name = name;
         this.odds = odds;
         this.continent = continent;
     };
 
+    // Constructor for Person with name, surname, and birthday
     function Person(name, surname, birthday) {
         this.name = name;
         this.surname = surname;
         this.birthday = birthday;
-        this.getData = function () {
-            const formattedDate = this.birthday.toLocaleDateString("sr-RS");
-            const shortDate = formattedDate.slice(0, -5) + this.birthday.getFullYear().toString().slice(-2);
-            return this.name + ", " + this.surname + ", " + shortDate;
 
+        // Method to get formatted data about the person
+        this.getData = function () {
+            // Format date to Serbian locale string (dd.mm.yyyy)
+            const formattedDate = this.birthday.toLocaleDateString("sr-RS");
+            // Shorten the year to last two digits and create a short date format
+            const shortDate = formattedDate.slice(0, -5) + this.birthday.getFullYear().toString().slice(-2);
+            // Return string with name, surname, and short date
+            return this.name + ", " + this.surname + ", " + shortDate;
         };
     };
 
+    // Constructor for Player: combines a Person, bet amount, and a Country
     function Player(person, amount, country) {
         this.person = person;
         this.amount = amount;
         this.country = country;
-        this.getProfile = function(){
+
+        // Method to get player's betting profile as a string
+        this.getProfile = function() {
+            // Calculate potential winning amount = odds * bet amount, rounded to 2 decimals
             const totalAmount = (this.country.odds * this.amount).toFixed(2);
+
+            // Calculate player's age from birthday
             const today = new Date();
             const birthYear = this.person.birthday.getFullYear();
             const age = today.getFullYear() - birthYear;
+
+            // Return string with country name, total possible winnings, player name, surname, and age
             return this.country.name + ", " + totalAmount + " eur, " + this.person.name + ", " + this.person.surname + ", " + age + " years";
         };
     };
 
+    // Constructor for Address with country, city, postal code, and street
     function Address(country, city, postal, street) {
         this.country = country;
         this.city = city;
         this.postal = postal;
         this.street = street;
+
+        // Method to get formatted full address string
         this.getAddress = function() {
             return this.street + ", " + this.postal + this.city + ", " + this.country;
         };
     };
 
+    // Constructor for BettingPlace, holding address and a list of players who bet there
     function BettingPlace(address, players) {
         this.address = address;
         this.players = [];
+
+        // Method to get formatted data about the betting place
         this.getData = function() {
+            // Extract street name without the house number (last word)
             const parts = this.address.street.split(" ");
             parts.pop();
             const streetName = parts.join(" ");
-            let totalBets = 0;
 
+            // Calculate total amount of all bets placed at this place
+            let totalBets = 0;
             for (var i = 0; i < this.players.length; i++) {
                 totalBets += this.players[i].amount;
             };
 
+            // Return string with street name, postal code, city, and sum of bets
             return streetName + ", " + this.address.postal + " " + this.address.city + ", sum of all bets: " + totalBets + "eur";
         };
+
+        // Method to add a player to the betting place
         this.addPlayer = function(player) {
             this.players.push(player);
         };
     };
 
+    // Constructor for BettingHouse, holds competition name, list of places, and total volume of bets
     function BettingHouse(competition, places, volume) {
         this.competition = competition;
         this.places = [];
         this.volume = 0;
     };
 
+    // Enum object for continents with fixed values
     const Continent = Object.freeze({
         ASIA: "AS",
         EUROPE: "EU",
@@ -77,54 +104,68 @@
         AUSTRALIA: "AU"
     });
 
+    // Factory function to create a Player from name, surname, birthday, bet amount, and country
     function createPlayer(name, surname, birthday, betAmount, country) {
         let person = new Person(name, surname, birthday);
         let player = new Player(person, betAmount, country);
         return player;
     };
 
+    // Factory function to create a BettingPlace from address details
     function createBettingPlace(country, city, postal, street) {
         let address = new Address(country, city, postal, street);
         let place = new BettingPlace(address);
         return place;
     };
 
+    // Create a BettingHouse for a competition
     const bettingHouse = new BettingHouse("Football World Cup Winner");
 
+    // Create some example countries with odds and continents
     const serbia = new Country("Serbia", 2.0, Continent.EUROPE);
     const brazil = new Country("Brazil", 3.0, Continent.SOUTH_AMERICA);
     const italy = new Country("Germany", 2.5, Continent.EUROPE);
 
+    // Create example players with their bets
     const player1 = createPlayer("Pera", "Peric", new Date(1995, 4, 15), 1050, serbia);
     const player2 = createPlayer("Danilo", "Stankovic", new Date(1992, 7, 22), 1200, brazil);
     const player3 = createPlayer("Milos", "Stanic", new Date(1989, 1, 5), 800, italy);
     const player4 = createPlayer("Ivan", "Dimitrovic", new Date(1998, 10, 3), 950, serbia);
 
+    // Create betting places with address info
     const place1 = createBettingPlace("Serbia", "Belgrade", 11000, "Nemanjina 4");
     const place2 = createBettingPlace("Serbia", "Belgrade", 11000, "Kralja Milana 10");
 
+    // Add places to betting house
     bettingHouse.places.push(place1, place2);
 
+    // Add players to places
     place1.players.push(player1, player2);
     place2.players.push(player3, player4);
 
+    // Set total number of bets (volume) in betting house
     bettingHouse.volume = place1.players.length + place2.players.length;
 
+    // Count how many bets are placed on Serbia
     let serbiaBets = 0;
     bettingHouse.places.forEach(function (place) {
-    place.players.forEach(function (player) {
-        if (player.country.name === "Serbia") {
-            serbiaBets++;
-           }
-     });
+        place.players.forEach(function (player) {
+            if (player.country.name === "Serbia") {
+                serbiaBets++;
+            }
+        });
     });
 
+    // Output summary of the betting house
     console.log(bettingHouse.competition + ", " + bettingHouse.places.length + " betting places, " + bettingHouse.volume + " bets");
 
+    // Output details for each betting place and the players who bet there
     bettingHouse.places.forEach(function (place) {
+        // Output betting place address and sum of bets
         console.log("    " + place.address.street + ", " + place.address.postal + " " + place.address.city + ", sum of all bets: " +
             place.players.reduce(function (sum, p) { return sum + p.amount; }, 0) + "eur");
-    
+
+        // Output each player with country code, possible win, name, and age
         place.players.forEach(function (player) {
             var age = new Date().getFullYear() - player.person.birthday.getFullYear();
             var winAmount = (player.amount * player.country.odds).toFixed(2);
@@ -132,7 +173,8 @@
                 player.person.name + " " + player.person.surname + ", " + age + " years");
         });
     });
-    
+
+    // Output total number of bets on Serbia
     console.log("There are " + serbiaBets + " bets on Serbia");
 
 })();
--- PP/Completed Exercises/OOP Exercises/shopping-list.js
+++ PP/Completed Exercises/OOP Exercises/shopping-list.js
@@ -1,102 +1,138 @@
 (function () {
 "use strict";
 
-
+// Constructor function to create a Product object
 function Product(name, price, expirationDate) {
+    // Generate a random product ID between 10000 and 99999
     this.productId = Math.floor(10000 + Math.random() * 90000);
     this.name = name;
+    // Round the price to 2 decimal places
     this.price = Math.round(parseFloat(price) * 100) / 100;
+    // Convert expirationDate string to a Date object
     this.expirationDate = new Date(expirationDate);
 
+    // Method to return product info string: product code, name and price
     this.getInfo = function() {
+        // Create product code from first and last letter of name + productId, all uppercase
         const productCode = (this.name[0] + this.name.slice(-1)).toUpperCase() + this.productId;
 
+        // Return formatted string with product code, name, and price
         return `${productCode}, ${this.name}, ${this.price}`;
     };
-};
+}
 
+// Constructor function to create a ShoppingBag object
 function ShoppingBag() {
+    // Initialize an empty array to store products
     this.listOfProducts = [];
 
+    // Method to add a product to the shopping bag if it's not expired
     this.addProduct = function(product) {
         const today = new Date();
 
+        // Only add product if expiration date is in the future
         if (product.expirationDate > today) {
             this.listOfProducts.push(product);
         };
     };
 
+    // Method to calculate average price of all products in the bag
     this.getAveragePrice = function() {
         let priceTotal = 0;
 
+        // Sum the prices of all products
         this.listOfProducts.forEach(function(product) {
             priceTotal += product.price;
         });
 
         const numberOfProducts = this.listOfProducts.length;
 
+        // If no products, return 0.000 as average
         if (numberOfProducts === 0) return "0.000";
 
+        // Calculate average and format to 3 decimal places
         const priceAverage = priceTotal / numberOfProducts;
 
         return priceAverage.toFixed(3);
-
     };
 
+    // Method to find and return info about the most expensive product in the bag
     this.getMostExpensive = function() {
         if (this.listOfProducts.length === 0) return null;
 
+        // Start assuming the first product is most expensive
         let mostExpensive = this.listOfProducts[0];
 
+        // Loop through products and update if find a more expensive one
         for (let i = 1; i < this.listOfProducts.length; i++) {
             if (mostExpensive.price < this.listOfProducts[i].price) {
                 mostExpensive = this.listOfProducts[i];
             };
         };
+
+        // Return the product info string of the most expensive product
         return mostExpensive.getInfo();
     };
 
+    // Method to calculate total price of all products in the bag
     this.calculateTotalPrice = function() {
         let totalPrice = 0;
 
+        // Sum the prices of all products
         for (let i = 0; i < this.listOfProducts.length; i++) {
             totalPrice += this.listOfProducts[i].price;
         };
 
         return totalPrice;
     };
-};
+}
 
+// Constructor function to create a PaymentCard object
 function PaymentCard(accountBalance, isActive, validUntil) {
+    // Round balance to 2 decimal places and convert to number
     this.accountBalance = Math.round(parseFloat(accountBalance).toFixed(2));
+    // Convert isActive value to boolean
     this.isActive = Boolean(isActive);
+    // Convert validUntil string to a Date object
     this.validUntil = new Date(validUntil);
-};
+}
 
+// Function to simulate checkout process and payment
 function checkoutAndBuy(shoppingBag, paymentCard) {
+    // Check if the card balance covers the total price of the shopping bag
     if (shoppingBag.calculateTotalPrice() <= paymentCard.accountBalance) {
+        // Successful purchase: return message with remaining balance
         return `Purchase is successful. Remaining balance is: ${paymentCard.accountBalance - shoppingBag.calculateTotalPrice()}`;
     } else {
+        // Insufficient funds: return message with amount missing
         return `Amount missing: ${shoppingBag.calculateTotalPrice() - paymentCard.accountBalance}`;
     };
-};
+}
 
+// Example products with name, price, and expiration date
 const product1 = new Product("banana", 1.99, "2025-12-23");
 const product2 = new Product("apple", 2.56, "2025-07-12");
 const product3 = new Product("carrot", 1.29, "2025-11-01");
 
-
+// Example payment card with balance, active status, and expiry date
 const creditCard = new PaymentCard(9.43, true, "2025-09-21");
 
+// Create a new shopping bag instance
 const shoppingBag = new ShoppingBag();
 
+// Add products to the shopping bag (only non-expired will be added)
 shoppingBag.addProduct(product1);
 shoppingBag.addProduct(product2);
 shoppingBag.addProduct(product3);
 
+// Output average price of products in the bag
 console.log("Average price:", shoppingBag.getAveragePrice());
+// Output info about the most expensive product in the bag
 console.log("Most Expensive Product:", shoppingBag.getMostExpensive());
-console.log("Total Price:", shoppingBag.calculateTotalPrice())
+// Output total price of all products in the bag
+console.log("Total Price:", shoppingBag.calculateTotalPrice());
 
+// Perform checkout and output result of purchase attempt
 console.log(checkoutAndBuy(shoppingBag, creditCard));
+
 })();
